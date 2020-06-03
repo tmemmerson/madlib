b9eef46923b102b10b048598dbefcce87d01c17c
Tristan Emmerson <tristan@stickerslug.com>
2020-06-02 21:12:49 -0700

    Add README.md formatting and ready form for use.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#formOne").submit(function(event) {
+        const person1Input = $("input#person1").val();
+        const person2Input = $("input#person2").val();
+        const animalInput= $("input#animal").val();
+        const exclamationInput = $("input#exclamation").val();
+        const verbInput = $("input#verb").val();
+        const nounInput = $("input#noun").val();
+
+      $(".person1").append(person1Input);
+      $(".person2").append(person2Input);
+      $(".animal").append(animalInput);
+      $(".exclamation").append(exclamationInput);
+      $(".verb").append(verbInput);
+      $(".noun").append(nounInput);
+  
+      $("#story").show();
+  
+      event.preventDefault();
+    });
+  });
