b8e38a8766a56f3dd1c2e038c54c74c8a332e527
Tristan Emmerson <tristan@stickerslug.com>
2020-06-02 21:14:32 -0700

    Finish exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
-        const person1Input = $("input#person1").val();
-        const person2Input = $("input#person2").val();
-        const animalInput= $("input#animal").val();
-        const exclamationInput = $("input#exclamation").val();
-        const verbInput = $("input#verb").val();
-        const nounInput = $("input#noun").val();
-
-      $(".person1").append(person1Input);
-      $(".person2").append(person2Input);
-      $(".animal").append(animalInput);
-      $(".exclamation").append(exclamationInput);
-      $(".verb").append(verbInput);
-      $(".noun").append(nounInput);
+      const person1Input = $("input#person1").val();
+      const person2Input = $("input#person2").val();
+      const animalInput= $("input#animal").val();
+      const exclamationInput = $("input#exclamation").val();
+      const verbInput = $("input#verb").val();
+      const nounInput = $("input#noun").val();
+  
+      $(".person1").text(person1Input);
+      $(".person2").text(person2Input);
+      $(".animal").text(animalInput);
+      $(".exclamation").text(exclamationInput);
+      $(".verb").text(verbInput);
+      $(".noun").text(nounInput);
   
       $("#story").show();
   
