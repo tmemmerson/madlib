a3d6bd7a74eb422d8bbd0bef4a2276cbfd079251
Tristan Emmerson <65921828+tmemmerson@users.noreply.github.com>
2020-07-06 17:30:52 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
       const exclamationInput = $("input#exclamation").val();
       const verbInput = $("input#verb").val();
       const nounInput = $("input#noun").val();
+      const scaryAnimalInput = $("input#x1").val();
+      const bigThingInput = $("input#x2").val();
+      const throwInput = $("input#x3").val();
+      const valuableInput = $("input#x4").val();
+      const placeInput = $("input#x5").val();
   
       $(".person1").text(person1Input);
       $(".person2").text(person2Input);
@@ -13,6 +18,11 @@ $(document).ready(function() {
       $(".exclamation").text(exclamationInput);
       $(".verb").text(verbInput);
       $(".noun").text(nounInput);
+      $(".x1").text(scaryAnimalInput);
+      $(".x2").text(bigThingInput);
+      $(".x3").text(throwInput);
+      $(".x4").text(valuableInput);
+      $(".x5").text(placeInput);
   
       $("#story").show();
   
